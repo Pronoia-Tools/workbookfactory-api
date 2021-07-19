d3636173460394822539197181e7251515bec6f7
Dave Merwin <dave@purebluedesign.com>
2021-07-19 12:13:11 -0700

    adding an email

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,3 +9,12 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 SESSION_CACHE_ALIAS = "default"
 
 DEBUG = True
+
+ADMINS = [('Dave Merwin', 'dave@purebluedesign.com'), ]
+DEFAULT_FROM_EMAIL = 'dave@purebluedesign.com'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'dave@purebluedesign.com'
+EMAIL_HOST_PASSWORD = 'euzzvdprkcggqfnj'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
