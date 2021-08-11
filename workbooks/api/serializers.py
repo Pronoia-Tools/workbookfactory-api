3396c912c524ddf8ae9bc9aa0ad3bc0adaf467ff
Dave Merwin <dave@purebluedesign.com>
2021-08-11 16:09:03 -0700

    tag serializer is working

--- workbooks/api/serializers.py
+++ workbooks/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from djmoney.contrib.django_rest_framework import MoneyField
+from taggit.serializers import (TagListSerializerField,
+                                TaggitSerializer)
 from users.api.serializers import AccountSerializer
 from ..models import Workbook, Chapter, Question, Answer
 
@@ -30,11 +32,12 @@ class ChapterSerializer(serializers.ModelSerializer):
         fields = ['id', 'owner', 'title', 'slug', 'front_matter', 'back_matter', 'content', 'archived', 'created', 'modified', 'question_set']
 
 
-class WorkbookSerializer(serializers.ModelSerializer):
+class WorkbookSerializer(TaggitSerializer, serializers.ModelSerializer):
+    tags = TagListSerializerField()
     owner = AccountSerializer(read_only=True)
     chapter_set = ChapterSerializer(many=True, required=False)
     price = MoneyField(max_digits=10, decimal_places=2)
 
     class Meta:
         model = Workbook
-        fields = ['id', 'owner', 'title', 'price', 'edition', 'language', 'description', 'slug', 'front_matter', 'back_matter', 'content', 'archived', 'created', 'modified', 'chapter_set', 'published', 'editable']
+        fields = ['id', 'owner', 'title', 'tags', 'price', 'edition', 'language', 'description', 'slug', 'front_matter', 'back_matter', 'content', 'archived', 'created', 'modified', 'chapter_set', 'published', 'editable']
